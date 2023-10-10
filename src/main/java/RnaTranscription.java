938f3515a48997b867180dcd0fe6aff769d5851c
Parth Purvesh <parth29dec@gmail.com>
2023-10-10 19:47:30 +0530

    Tests passed

--- src/main/java/RnaTranscription.java
+++ src/main/java/RnaTranscription.java
@@ -1,7 +1,21 @@
-class RnaTranscription {
+import java.util.stream.Collectors;
 
+class RnaTranscription {
     String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+//        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String rnaStrand = "";
+        for(int i=0;i<dnaStrand.length();i++) {
+            char word = dnaStrand.charAt(i);
+            if (word == 'G')
+                rnaStrand += 'C';
+            if (word == 'C')
+                rnaStrand += 'G';
+            if (word == 'T')
+                rnaStrand += 'A';
+            if (word == 'A')
+                rnaStrand += 'U';
+        }
+        return rnaStrand;
     }
 
 }
